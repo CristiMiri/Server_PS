f720b9b8907a0d4f93b482d9551601b9fb03abd3
Miritoiu Cristian <cristianmiritoiu@yahoo.com>
2024-05-31 13:05:09 +0300

    server change

---
+++ Domain/DTO/PackageDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server.Domain.DTO
+{
+    internal class PackageDTO
+    {
+        public bool Result {  get; set; }
+        public string Message {  get; set; }
+        public object Data {  get; set; }
+
+        public PackageDTO() { }
+    }
+}
